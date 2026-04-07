77318955f3941445989ea413a22fe340c1ccf660
dexeme <dexe.me12@gmail.com>
2026-04-07 16:44:01 -0300

    Formata conteúdo latex da página boolean_functions

--- docs/conf.py
+++ docs/conf.py
@@ -29,6 +29,27 @@ pygments_style = 'sphinx'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
+# https://github.com/flintlib/flint/blob/main/doc/source/conf.py
+latex_elements = {
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    'papersize': 'a4paper',
+    'fontpkg': '',
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+    'preamble': '\\usepackage{lmodern}\n\\setcounter{tocdepth}{2}\n\\urlstyle{tt}',
+    'preamble': '\\usepackage{mathrsfs}\n\\usepackage{lmodern}\n\\setcounter{tocdepth}{2}\n\\urlstyle{tt}',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
+}
+
 html_theme = 'classic'
 html_static_path = ['_static']
 html_theme_options = {
